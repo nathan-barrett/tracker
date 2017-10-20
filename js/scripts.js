669e620a325c2f5d475ccf7c0e3b1ef6835ef4ae
Nathan Barrett <demeules.barrett@gmail.com>
2017-10-20 13:40:36 -0700

    add header and center text

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $('document').ready(function() {
   };
   $('form#tracker').submit(function(event) {
     event.preventDefault();
+    var inputName = $("input#nameInput").val();
     var quesOne = parseInt($("select#describeInput").val());
     var quesTwo = parseInt($("select#goalInput").val());
     var quesThree = parseInt($("select#interestInput").val());
@@ -11,12 +12,24 @@ $('document').ready(function() {
     var quesFive = parseInt($("select#bugInput").val());
     var totalQuesVal = addQuesVal(quesOne, quesTwo, quesThree, quesThree, quesFour, quesFive);
     if (totalQuesVal >= 5 && totalQuesVal <= 7) {
+      $("#defaultName").hide();
+      $("#surveyName").text(inputName);
+      $("#cssDesignResult").hide();
+      $("#rubyRailsResult").hide();
       $("#cSharpResult").show();
     }
     else if (totalQuesVal >= 8 && totalQuesVal <= 12) {
+      $("#defaultName").hide();
+      $("#surveyName").text(inputName);
+      $("#cSharpResult").hide();
+      $("#rubyRailsResult").hide();
       $("#cssDesignResult").show();
     }
     else if (totalQuesVal >= 12) {
+      $("#defaultName").hide();
+      $("#surveyName").text(inputName);
+      $("#cSharpResult").hide();
+      $("#cssDesignResult").hide();
       $("#rubyRailsResult").show();
 
     }
