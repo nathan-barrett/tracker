7d9353e3ccd1e61d3f8a08a4ff31de1c9116e998
Nathan Barrett <demeules.barrett@gmail.com>
2017-10-20 12:42:58 -0700

    fix a stupid problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
-var addQuesVal = function (_ques1, _ques2, _ques3, _ques4, _ques5) {
-  return _ques1 + _ques2 + _ques3 + _ques4 + _ques5;
-};
 $('document').ready(function() {
-
-
+  var addQuesVal = function (_ques1, _ques2, _ques3, _ques4, _ques5) {
+    return _ques1 + _ques2 + _ques3 + _ques4 + _ques5;
+  };
   $('form#tracker').submit(function(event) {
-    var ques1 = parseInt($("select#describeInput").val());
-    var ques2 = parseInt($("select#goalInput").val());
-    var ques3 = parseInt($("select#interestInput").val());
-    var ques4 = parseInt($("select#foodInput").val());
-    var ques5 = parseInt($("select#bugInput").val());
-
-    var totalQuesVal = ques1 + ques2 + ques3 + ques4 + ques5;
-
-    console.log(totalQuesVal);
     event.preventDefault();
+    var quesOne = parseInt($("select#describeInput").val());
+    var quesTwo = parseInt($("select#goalInput").val());
+    var quesThree = parseInt($("select#interestInput").val());
+    var quesFour = parseInt($("select#foodInput").val());
+    var quesFive = parseInt($("select#bugInput").val());
+    var totalQuesVal = addQuesVal(quesOne, quesTwo, quesThree, quesThree, quesFour, quesFive);
+    if (totalQuesVal >= 5 && totalQuesVal <= 7) {
+      $("#cSharpResult").show();
+    }
+    else if (totalQuesVal >= 8 && totalQuesVal <= 12) {
+      $("#cssDesignResult").show();
+    }
+    else if (totalQuesVal >= 12) {
+      $("#rubyRailsResult").show();
+
+    }
+
   });
 
 
